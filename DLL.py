35308e33a59dd1088dd357876a2e8594fb603d18
Ankit Kumar <137291083+Ankit-0512-kumar@users.noreply.github.com>
2024-02-23 17:15:22 +0530

    Update DLL.py

--- DLL.py
+++ DLL.py
@@ -3,7 +3,7 @@ class Node:
     def __init__(self,prev=None,item=None,next=None):
         self.prev = prev
         self.item = item
-        self.next = next
+        self.next = nex
 
 
 class DLL:
